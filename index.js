e86947c41bc56d1c0ad9cf129d14a10c99bccbac
Jan Riehn <jan.riehn@web.de>
2016-04-15 17:21:59 +0200

    Viola / @bkate / Jan - first shot of happinessradar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var allHappyCalls = [];
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,11 +12,40 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var currentHappiNess = 8;
+    response.render('pages/index', {
+        currentHappiNess: currentHappiNess
+    });
+});
+
+app.get('/status', function(req, res) {
+    var total = 0;
+    for (var i in allHappyCalls) {
+      total += parseInt(allHappyCalls[i]);
+    }
+
+    var status = {
+      "count": allHappyCalls.length,
+      "average" : total / allHappyCalls.length
+    };
+
+    res.send(status);
+});
+
+app.get('/submit', function (req, res) {
+    allHappyCalls.push(req.query.happiness);
+
+    var total = 0;
+    for (var i in allHappyCalls) {
+      total += parseInt(allHappyCalls[i]);
+    }
+
+    console.log("Summe:" + total);
+    console.log("Average:" + total / allHappyCalls.length);
+
+    res.send('A message!');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
